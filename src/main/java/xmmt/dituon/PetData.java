0b8d8cc8b397752c2bea5dc939cfd2cabab05f07
hundun <wwj.hddjj@gmail.com>
2022-05-31 13:22:17 +0800

    plugin进一步使用share

--- src/main/java/xmmt/dituon/PetData.java
+++ src/main/java/xmmt/dituon/PetData.java
@@ -4,13 +4,15 @@ import kotlinx.serialization.json.JsonArray;
 import kotlinx.serialization.json.JsonElement;
 import net.mamoe.mirai.contact.Group;
 import net.mamoe.mirai.contact.Member;
+import net.mamoe.mirai.message.data.Image;
+import net.mamoe.mirai.utils.ExternalResource;
 import xmmt.dituon.share.*;
 
-import java.util.Objects;
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
 import java.util.Random;
 
-import static xmmt.dituon.GifMaker.makeGIF;
-import static xmmt.dituon.ImageMaker.makeImage;
+import static xmmt.dituon.share.ImageSynthesis.getAvatarImage;
 
 public class PetData extends BasePetData {
 
@@ -31,69 +33,21 @@ public class PetData extends BasePetData {
     }
 
     public static void sendImage(Group group, Member from, Member to, String key) {
-        if (!dataMap.containsKey(key)) {
-            System.out.println("无效的key: " + key);
-            sendImage(group, from, to);
-        }
-        DataJSON data = dataMap.get(key);
-        key = dataRoot.getAbsolutePath() + key + "/";
+        BufferedImage fromAvatarImage = getAvatarImage(from.getAvatarUrl());
+        BufferedImage toAvatarImage = getAvatarImage(to.getAvatarUrl());
+
+        InputStream generatedImage = generateImage(fromAvatarImage, toAvatarImage, key);
 
         try {
-            if (data.getType() == Type.GIF) {
-                if (data.getAvatar() == Avatar.SINGLE) {
-                    int[][] pos = new int[data.getPos().getSize()][4];
-
-                    int i = 0;
-                    for (JsonElement je : data.getPos()) {
-                        pos[i++] = JsonArrayToIntArray((JsonArray) je);
-                    }
-
-                    group.sendMessage(Objects.requireNonNull(makeGIF(to, key, pos,
-                            data.getAvatarOnTop(), data.getRotate(), data.getRound())));
-                    return;
-                }
-                if (data.getAvatar() == Avatar.DOUBLE) {
-                    JsonArray fromJa = (JsonArray) data.getPos().get(0);
-                    JsonArray toJa = (JsonArray) data.getPos().get(1);
-
-                    int[][] fromPos = new int[fromJa.getSize()][4];
-                    int[][] toPos = new int[toJa.getSize()][4];
-
-                    int i = 0;
-                    for (JsonElement fromJe : fromJa) {
-                        fromPos[i++] = JsonArrayToIntArray((JsonArray) fromJe);
-                    }
-                    i = 0;
-                    for (JsonElement toJe : toJa) {
-                        toPos[i++] = JsonArrayToIntArray((JsonArray) toJe);
-                    }
-
-                    group.sendMessage(Objects.requireNonNull(makeGIF(
-                            from, to, key, fromPos, toPos,
-                            data.getAvatarOnTop(), data.getRotate(), data.getRound())));
-                    return;
-                }
-            }
-
-            if (data.getType() == Type.IMG){
-                if (data.getAvatar() == Avatar.SINGLE) {
-                    int[] pos = JsonArrayToIntArray(data.getPos());
-
-                    group.sendMessage(Objects.requireNonNull(makeImage(to, key, pos,
-                            data.getAvatarOnTop(), data.getRotate(), data.getRound())));
-                    return;
-                }
-                if (data.getAvatar() == Avatar.DOUBLE) {
-                    int[] pos1 = JsonArrayToIntArray((JsonArray) data.getPos().get(0));
-                    int[] pos2 = JsonArrayToIntArray((JsonArray) data.getPos().get(1));
-
-                    group.sendMessage(Objects.requireNonNull(makeImage(from, to, key, pos1, pos2,
-                            data.getAvatarOnTop(), data.getRotate(), data.getRound())));
-                    return;
-                }
+            if (generatedImage != null) {
+                ExternalResource resource = ExternalResource.create(generatedImage);
+                Image image = to.uploadImage(resource);
+                group.sendMessage(image);
+            } else {
+                System.out.println("生成图片失败");
             }
         } catch (Exception ex) {
-            System.out.println("解析 " + key + "/data.json 出错");
+            System.out.println("发送生成的图片时出错：" + ex.getMessage());
             ex.printStackTrace();
         }
     }
