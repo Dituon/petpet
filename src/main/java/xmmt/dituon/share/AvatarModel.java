b14e16842b0a45032a362b7cc710df54292dad21
Dituon <2544193782@qq.com>
2022-06-18 12:05:43 +0800

    fix 3.0

---
+++ src/main/java/xmmt/dituon/share/AvatarModel.java
@@ -0,0 +1,131 @@
+package xmmt.dituon.share;
+
+import kotlinx.serialization.json.JsonArray;
+import kotlinx.serialization.json.JsonElement;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class AvatarModel {
+    protected AvatarType type;
+    protected int[][] pos = {{0, 0, 100, 100}};
+    protected String format;
+    protected int angle;
+    protected boolean round;
+    protected boolean rotate;
+    protected boolean onTop;
+    protected BufferedImage image = null;
+    private int posIndex = 0;
+    private boolean antialias = false;
+
+    public AvatarModel(AvatarData data, AvatarExtraData extraData, Type imageType) {
+        type = data.getType();
+        setImage(type, extraData);
+        setPos(data.getPos(), imageType);
+        format = data.getFormat() != null ? data.getFormat() : "png";
+        angle = data.getAngle() != null ? data.getAngle() : 0;
+        round = Boolean.TRUE.equals(data.getRound());
+        rotate = Boolean.TRUE.equals(data.getRotate());
+        onTop = Boolean.TRUE.equals(data.getAvatarOnTop());
+        antialias = Boolean.TRUE.equals(data.getAntialias());
+    }
+
+    private void setImage(AvatarType type, AvatarExtraData extraData) {
+        switch (type) {
+            case FROM:
+                image = extraData.getFromAvatarUrl() != null ?
+                        ImageSynthesis.getAvatarImage(extraData.getFromAvatarUrl()) : null;
+                break;
+            case TO:
+                image = extraData.getToAvatarUrl() != null ?
+                        ImageSynthesis.getAvatarImage(extraData.getToAvatarUrl()) : null;
+                break;
+            case GROUP:
+                image = extraData.getGroupAvatarUrl() != null ?
+                        ImageSynthesis.getAvatarImage(extraData.getGroupAvatarUrl()) : null;
+                break;
+            case BOT:
+                image = extraData.getBotAvatarUrl() != null ?
+                        ImageSynthesis.getAvatarImage(extraData.getBotAvatarUrl()) : null;
+                break;
+        }
+    }
+
+    private void setPos(JsonArray posElements, Type imageType) {
+        int i = 0;
+        switch (imageType) {
+            case GIF:
+                pos = new int[posElements.size()][4];
+                for (JsonElement je : posElements) {
+                    pos[i++] = JsonArrayToIntArray((JsonArray) je);
+                }
+                break;
+            case IMG:
+                pos[i] = JsonArrayToIntArray(posElements);
+                break;
+        }
+    }
+
+    private int[] JsonArrayToIntArray(JsonArray ja) {
+        return new int[]{
+                Integer.parseInt(ja.get(0).toString()),
+                Integer.parseInt(ja.get(1).toString()),
+                Integer.parseInt(ja.get(2).toString()),
+                Integer.parseInt(ja.get(3).toString())
+        };
+    }
+
+    public AvatarType getType() {
+        return type;
+    }
+
+    public int[][] getPos() {
+        return pos;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public int getAngle() {
+        return angle;
+    }
+
+    public boolean isRound() {
+        return round;
+    }
+
+    public boolean isRotate() {
+        return rotate;
+    }
+
+    public boolean isOnTop() {
+        return onTop;
+    }
+
+    public BufferedImage getImage() {
+        assert image != null;
+        if (rotate) {
+            try {
+                return ImageSynthesis.convertCircular(image, antialias);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return image;
+    }
+
+    public int getRotateIndex() {
+        if (!rotate) {
+            return 0;
+        }
+        return posIndex;
+    }
+
+    public int[] nextPos() {
+        if (posIndex > pos.length) {
+            return new int[]{0, 0, 0, 0};
+        }
+        return pos[posIndex++];
+    }
+}
--- src/main/java/xmmt/dituon/share/ImageSynthesis.java
+++ src/main/java/xmmt/dituon/share/ImageSynthesis.java
@@ -5,57 +5,72 @@ import org.jetbrains.annotations.Nullable;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.font.TextAttribute;
 import java.awt.geom.Ellipse2D;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.text.AttributedCharacterIterator;
-import java.text.AttributedString;
 import java.util.ArrayList;
 
 public class ImageSynthesis {
 
+    public static BufferedImage synthesisImage(BufferedImage sticker,
+                                               ArrayList<AvatarModel> avatarList, ArrayList<TextModel> textList,
+                                               boolean antialias) {
+        BufferedImage output = new BufferedImage(sticker.getWidth(), sticker.getHeight(), sticker.getType());
+        Graphics2D g2d = output.createGraphics();
 
-    /**
-     * 至多两个（可null）头像合成，不旋转，带文字
-     */
-    public static BufferedImage synthesisImage(BufferedImage sticker, BufferedImage avatarImage1, BufferedImage avatarImage2,
-                                               int[] pos1, int[] pos2, boolean isAvatarOnTop,boolean antialias, ArrayList<TextModel> texts) {
-        return synthesisImage(sticker, avatarImage1, avatarImage2, pos1, pos2, 0, isAvatarOnTop, antialias, texts);
+        if (antialias) { //抗锯齿
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        }
+
+        // 背景
+        g2d.setColor(Color.WHITE);
+        g2d.fillRect(0, 0, sticker.getWidth(), sticker.getHeight());
+        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, 1.0F));
+
+        // 按照图层分类
+        ArrayList<AvatarModel> topAvatars = new ArrayList<>();
+        ArrayList<AvatarModel> bottomAvatars = new ArrayList<>();
+        for (AvatarModel avatar : avatarList) {
+            if (avatar.isOnTop()) {
+                topAvatars.add(avatar);
+            } else {
+                bottomAvatars.add(avatar);
+            }
+        }
+        // 画
+        for (AvatarModel avatar : bottomAvatars) {
+            g2dDrawAvatar(g2d, avatar.getImage(), avatar.nextPos(), avatar.getRotateIndex(), avatar.getAngle());
+        }
+        g2d.drawImage(sticker, 0, 0, sticker.getWidth(), sticker.getHeight(), null);
+        for (AvatarModel avatar : topAvatars) {
+            g2dDrawAvatar(g2d, avatar.getImage(), avatar.nextPos(), avatar.getRotateIndex(), avatar.getAngle());
+        }
+
+        g2dDrawTexts(g2d, textList);
+        g2d.dispose();
+        return output;
     }
 
-    /**
-     * 至多两个（可null）头像合成，旋转
-     */
-    public static BufferedImage synthesisImage(BufferedImage sticker, BufferedImage avatarImage1, BufferedImage avatarImage2,
-                                               int[] pos1, int[] pos2,
-                                               int rotateIndex, boolean isAvatarOnTop, boolean antialias) {
-        return synthesisImage(sticker, avatarImage1, avatarImage2, pos1, pos2, rotateIndex, isAvatarOnTop, antialias, null);
-    }
-
-    // 至多两个（可null）头像合成，旋转，带文字
-    public static BufferedImage synthesisImage(BufferedImage sticker, BufferedImage avatarImage1, BufferedImage avatarImage2,
-                                               int[] pos1, int[] pos2,
-                                               int rotateIndex, boolean isAvatarOnTop, boolean antialias,
-                                               ArrayList<TextModel> texts) {
-        return synthesisImageGeneral(sticker, avatarImage1, avatarImage2, pos1, pos2, rotateIndex, isAvatarOnTop, antialias, texts);
-    }
-
-    private static void g2dDrawAvatar(Graphics2D g2d, BufferedImage avatarImage, int[] pos, int rotateIndex) {
+    private static void g2dDrawAvatar(Graphics2D g2d, BufferedImage avatarImage, int[] pos, int rotateIndex, int angle) {
         if (avatarImage == null) {
             return;
         }
         BufferedImage newAvatarImage = new BufferedImage(avatarImage.getWidth(), avatarImage.getHeight(), avatarImage.getType());
-        if (rotateIndex != 0) {
+
+        if (rotateIndex == 0 && angle == 0){
+            newAvatarImage = avatarImage;
+        } else {
             Graphics2D rotateG2d1 = newAvatarImage.createGraphics();
-            rotateG2d1.rotate(Math.toRadians((float) (360 / pos.length) * (rotateIndex + 1)),
+            //TODO 旋转时会有黑边 应当使用AffineTransform
+            rotateG2d1.rotate(Math.toRadians((float) ((360 / pos.length) * (rotateIndex)) + angle),
                     avatarImage.getWidth() / 2, avatarImage.getHeight() / 2);
             rotateG2d1.drawImage(avatarImage, null, 0, 0);
-        } else {
-            newAvatarImage = avatarImage;
         }
+
         int x = pos[0];
         int y = pos[1];
         int w = pos[2];
@@ -72,46 +87,6 @@ public class ImageSynthesis {
             }
         }
     }
-
-    private static BufferedImage synthesisImageGeneral(
-            BufferedImage sticker,
-            @Nullable BufferedImage avatarImage1,
-            @Nullable BufferedImage avatarImage2,
-            @Nullable int[] pos1, @Nullable int[] pos2,
-            int rotateIndex,
-            boolean isAvatarOnTop,
-            boolean antialias,
-            @Nullable ArrayList<TextModel> texts
-    ) {
-
-        BufferedImage output = new BufferedImage(sticker.getWidth(), sticker.getHeight(), sticker.getType());
-        Graphics2D g2d = output.createGraphics();
-
-        if (antialias) { //抗锯齿
-            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        }
-
-        // 背景
-        g2d.setColor(Color.WHITE);
-        g2d.fillRect(0, 0, sticker.getWidth(), sticker.getHeight());
-        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, 1.0F));
-        // 按图层顺序依次添加，方便直观调整顺序
-        if (isAvatarOnTop) {
-            g2d.drawImage(sticker, 0, 0, sticker.getWidth(), sticker.getHeight(), null);
-            g2dDrawAvatar(g2d, avatarImage1, pos1, rotateIndex);
-            g2dDrawAvatar(g2d, avatarImage2, pos2, rotateIndex);
-        } else {
-            g2dDrawAvatar(g2d, avatarImage1, pos1, rotateIndex);
-            g2dDrawAvatar(g2d, avatarImage2, pos2, rotateIndex);
-            g2d.drawImage(sticker, 0, 0, sticker.getWidth(), sticker.getHeight(), null);
-        }
-        g2dDrawTexts(g2d, texts);
-
-        g2d.dispose();
-        return output;
-    }
-
 
     public static BufferedImage convertCircular(BufferedImage input, boolean antialias) throws IOException {
         BufferedImage output = new BufferedImage(input.getWidth(), input.getHeight(), BufferedImage.TYPE_4BYTE_ABGR);
@@ -138,7 +113,13 @@ public class ImageSynthesis {
             image = ImageIO.read(conn.getInputStream());
             conn.disconnect();
         } catch (Exception e) {
-            System.out.println("获取头像失败\nHttpURLConnection: " + conn + "\nURL: " + URL);
+            try {
+                return ImageIO.read(new File(URL));
+            } catch (IOException ioex) {
+                System.out.println("获取本地头像失败\nPath: " + URL);
+                e.printStackTrace();
+            }
+            System.out.println("获取网络头像失败\nHttpURLConnection: " + conn + "\nURL: " + URL);
             e.printStackTrace();
         } finally {
             assert conn != null;
