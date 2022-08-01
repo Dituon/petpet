463c73719d55c2ebb8c0461cdab25bf98105a464
47479380 <2752181597@qq.com>
2022-08-01 22:47:17 +0800

    编辑器addAvatar设置默认宽高

--- editor/avatar.js
+++ editor/avatar.js
@@ -10,6 +10,10 @@ function Avatar(qq = 2544193782) {
         })
         that.avatar.uniformScaling = true
         canvas.add(that.avatar);
+        let pos = [0, 0,
+            Math.round(that.avatar.getScaledWidth()), Math.round(that.avatar.getScaledHeight())]
+        $('#avatar_pos').text('坐标: [' + pos + ']')
+        that.setPos(pos)
         that.avatar.on('moving', () => {
             avatarMoving()
         })
