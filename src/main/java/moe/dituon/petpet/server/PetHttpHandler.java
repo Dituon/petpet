e29f83ef0a5952cae420e199ecd1874c03fe2408
Dituon <2544193782@qq.com>
2023-01-28 23:55:28 +0800

    高性能 form-data parser

--- src/main/java/moe/dituon/petpet/server/PetHttpHandler.java
+++ src/main/java/moe/dituon/petpet/server/PetHttpHandler.java
@@ -1,5 +1,6 @@
 package moe.dituon.petpet.server;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
@@ -8,10 +9,12 @@ import java.nio.charset.StandardCharsets;
 
 public class PetHttpHandler implements HttpHandler {
     private final ServerPetService service;
-    PetHttpHandler(ServerPetService service){
+
+    PetHttpHandler(ServerPetService service) {
         super();
         this.service = service;
     }
+
     @Override
     public void handle(HttpExchange httpExchange) {
         try {
@@ -26,28 +29,38 @@ public class PetHttpHandler implements HttpHandler {
 
                 System.out.println("DEBUG[GET URL]: " + requestParam);
                 parser = new GETParser(service, requestParam);
-            } else {
-                //POST
-                BufferedReader bufferedReader = new BufferedReader(
-                        new InputStreamReader(httpExchange.getRequestBody(), StandardCharsets.UTF_8)
-                );
-
-                StringBuilder requestBodyContent = new StringBuilder();
-                String line;
-                while ((line = bufferedReader.readLine()) != null) {
-                    requestBodyContent.append(line);
-                }
-
-                String content = requestBodyContent.toString();
-                System.out.println("DEBUG[POST BODY]: " + content);
-                parser = new POSTParser(service, content);
+                finish(httpExchange, parser);
+                return;
             }
 
-            handleResponse(httpExchange, parser.getImagePair().getFirst(), parser.getImagePair().getSecond());
-            parser.close();
+            // POST
+            Headers headers = httpExchange.getRequestHeaders();
+            String contentType = headers.getFirst("Content-Type");
 
-        } catch (Exception e) {
-            e.printStackTrace();
+            if (contentType.startsWith("multipart/form-data")) {
+                parser = new FormDataParser(service, httpExchange);
+
+                finish(httpExchange, parser);
+                return;
+            }
+
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(httpExchange.getRequestBody(), StandardCharsets.UTF_8)
+            );
+
+            StringBuilder requestBodyContent = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                requestBodyContent.append(line);
+            }
+
+            String content = requestBodyContent.toString();
+            System.out.println("DEBUG[POST BODY]: " + content);
+            parser = new POSTParser(service, content);
+            finish(httpExchange, parser);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
             handleResponse(httpExchange, 400);
         }
     }
@@ -74,7 +87,7 @@ public class PetHttpHandler implements HttpHandler {
         out.close();
     }
 
-    private void handleResponse(HttpExchange httpExchange, String responseJson) throws Exception {
+    private void handleResponse(HttpExchange httpExchange, String responseJson) throws IOException {
         byte[] responseContentByte = responseJson.getBytes(StandardCharsets.UTF_8);
         httpExchange.getResponseHeaders().add("Content-Type:", "application/json;charset=utf-8");
         httpExchange.sendResponseHeaders(200, responseContentByte.length);
@@ -83,6 +96,11 @@ public class PetHttpHandler implements HttpHandler {
         out.write(responseContentByte);
         out.flush();
         out.close();
+    }
+
+    private void finish(HttpExchange httpExchange, RequestParser parser) throws IOException {
+        handleResponse(httpExchange, parser.getImagePair().getFirst(), parser.getImagePair().getSecond());
+        parser.close();
     }
 }
 
